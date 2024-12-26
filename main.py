e34069b38fa90ef4ace444ed5eb80202ecfa4536
Fang, Xin Ran <115848928+Heimerstinger@users.noreply.github.com>
2024-12-26 17:40:54 -0500

    add case 'complete'

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ").strip()
- 
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
+
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
@@ -10,13 +10,17 @@ while True:
         case 'show':
             for index, item in enumerate(todos):
                 item = item.title()
-                print(f"{index}-{item}")
+                print(f"{index + 1}-{item}")
             # todos.index("xxx")
         case 'edit':
             number = int(input("Enter the number of the todo to edit: "))
             number -= 1
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Enter the number of the todo to complete: "))
+            number -= 1
+            todos.pop(number)
         case 'exit':
             break
         case whatever:
