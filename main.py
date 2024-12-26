bc1139ec4fb1ceeaa972eefc8ca3a02287e30cab
Fang, Xin Ran <115848928+Heimerstinger@users.noreply.github.com>
2024-12-26 17:01:02 -0500

    create main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ todos = []
 
 while True:
     user_action = input("Type add, show, edit or exit: ").strip()
-
+ 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
