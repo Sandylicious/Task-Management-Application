1b2456f2373ae9d7db5eb0796565426b7042994b
Fang, Xin Ran <115848928+Heimerstinger@users.noreply.github.com>
2024-12-27 11:54:41 -0500

    refactor

--- main.py
+++ main.py
@@ -18,22 +18,10 @@ while True:
 
             todos.append(todo)
 
-            # method 1
-            # file = open("todos.txt", "w")
-            # file.writelines(todos)
-            # file.close()
-
-            # method 2
             with open("todos.txt", "w") as file:
                 file.writelines(todos)
 
         case 'show':
-            # method 1
-            # file = open("todos.txt", "r")
-            # todos = file.readlines()
-            # file.close()
-
-            # method 2
             with open("todos.txt", "r") as file:
                 todos = file.readlines()
 
@@ -60,18 +48,28 @@ while True:
             number = int(input("Enter the number of the todo to edit: "))
             number -= 1
             new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            todos[number] = new_todo + '\n'
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
         case 'complete':
             number = int(input("Enter the number of the todo to complete: "))
             with open("todos.txt", "r") as file:
                 todos = file.readlines()
 
+            todo_completed = todos[number - 1].strip("\n").title()
             todos.pop(number - 1)
+            # todos.remove("xxx")
 
             with open("todos.txt", "w") as file:
                 file.writelines(todos)
 
-            # todos.remove("xxx")
+            message = f"Todo [{todo_completed}] was removed from the list."
+            print(message)
         case 'exit':
             break
         case whatever:
